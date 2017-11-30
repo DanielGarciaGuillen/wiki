5b19542eec1e877028911380f9a6cf474c5eb256
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-11-29 22:13:40 -0500

    added logic

--- javascript.js
+++ javascript.js
@@ -1,31 +1,23 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert%20Einstein&srlimit=10";
 
+var title = document.getElementById('quote');
 
 var arrayList=[];
 
 fetch(proxyurl + url).then(function(response){
     return response.json();
-    /* if(response.ok){
-        
-    } throw new Error("Network response was not ok.");     */
+   
 }).then(function(json){
     console.log("parsed json", json)
-    let output = '<h2 List of content</h2>';
-     Object.keys(json).forEach(function(query) {        
-         arrayList = json[query];
-        console.log(arrayList);
+    var list = json.query.search;
+    console.log(list);
+    list.forEach(function(k){
+        console.log( k.title + k.snippet)
+       
     });
-
-    /*
-    json.forEach((query)=>{
-        result +=
-        `<ul>
-        <li> ${query.search}</li>
-        </ul>
-        `;
-        document.getElementById("list").innerHTML = result;
-    }) */
+   
+  
     
 }).catch(function(error){
     console.log("There has been a problem" + error.message)
