188600a8ecfab8b1ba6f68e6c90760c69e218d96
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-11-30 15:27:56 -0500

    new logic

--- javascript.js
+++ javascript.js
@@ -1,22 +1,75 @@
+
+const searchInput = document.querySelector('.search');
+var searchWord =  document.querySelector('.search').innerHTML;
+
+const suggestions = document.querySelector('.suggestions');
+console.log(searchWord);
+
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert%20Einstein&srlimit=10";
+var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=" + searchWord + "&srlimit=10"; /* Albert%20Einstein; */
 
 var titleid = document.getElementById("titleId").innerHtml;
 console.log(titleid);
 
-var arrayList=[];
 
+
+var list =[] ;
+
+
+function apiSearch(){fetch(proxyurl + url)
+    .then(blob=>blob.json())
+    .then(data=> list.push(...data.query.search));
+
+    function findMatches(wordToMatch, list){
+        return list.filter(search => {      
+        const regex = new RegExp(wordToMatch, 'gi');
+        return search.title.match(regex)
+    });
+}
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, list)
+    const html = matchArray.map(search =>{
+        return`
+        <li>
+        <span class="title">${search.title}, ${search.snippet}</span>
+        </li>
+        `;
+    });
+    suggestions.innerHTML = html;
+}
+
+
+
+/* searchInput.addEventListener('change', displayMatches); */
+searchInput.addEventListener('keyup', displayMatches);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 fetch(proxyurl + url).then(function(response){
     return response.json();
    
 }).then(function(json){
-   /*  console.log("parsed json", json) */
-    var list = json.query.search;
-    /* console.log(list); */
+     console.log("parsed json", json) 
+    list = json.query.search;
+    console.log(list); 
     list.forEach(function(k){
-        /* console.log( k.title + k.snippet) */
-        titleid = k.title;
-        console.log( k.title)
+        / console.log( k.title + k.snippet)
+       
+        console.log( list )
        
     });
    
@@ -24,4 +77,4 @@ fetch(proxyurl + url).then(function(response){
     
 }).catch(function(error){
     console.log("There has been a problem" + error.message)
-});
+}); */
