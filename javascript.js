578f96a5fbcb4b48280cfac3f7ea46dea5f1b8f6
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2017-12-01 23:56:16 -0500

    new logic

--- javascript.js
+++ javascript.js
@@ -1,23 +1,25 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert%20Einstein&srlimit=10"; /* Albert%20Einstein; */
-
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch="/* Albert%20Einstein;"  Albert%20Einstein;  */
+const urlEnd = "&srlimit=10"
 var list =[];
 
-fetch(proxyurl + url)
-    .then(blob=>blob.json())
-    .then(data=> list.push(...data.query.search));
 
+function callApi(){
+ fetch(proxyUrl + url+ this.value+ urlEnd)
+    .then(blob=>blob.json())    
+    .then(data=> list.push(...data.query.search));
         function findMatches(wordToMatch, list){
+            console.log(wordToMarch + list)
             return list.filter(search => {      
             const regex = new RegExp(wordToMatch, 'gi');
-            return search.title.match(regex)
+            return search.title.match(regex)  
+                   
     });
     }
+}
 
-
-function displayMatches(){
-    console.log(this.value);
-    const matchArray = findMatches(this.value, list)
+function displayMatches(){    
+    const matchArray = findMatches(wordToMatch, list);   
     const html = matchArray.map(search =>{
         return`
         <li>
@@ -28,14 +30,18 @@ function displayMatches(){
     suggestions.innerHTML = html;
 }
 
+
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
 
 searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('keyup', callApi);
+searchInput.addEventListener('keydown', deleteList);
 
-
-
+function deleteList(){
+    list.length = 0;
+}
 
 
 
